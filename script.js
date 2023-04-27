fc4ab06e350c58e4c68bc98cbe35ffd6c542bbab
Tariqyy <131998471+Tariqyy@users.noreply.github.com>
2023-04-27 16:50:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let option1 = document.querySelector(".option-one-screen");
+let option2 = document.querySelector(".option-two-screen");
+let option3 = document.querySelector("option-three-end")
+let option4 = document.querySelector("option-four-end")
+let storyopening = document.querySelector(".story-opening");
+let button = document.querySelector(".buttons")
+let title = document.querySelector("title")
+
+
+let option1button = document.querySelector(".option-one");
+let option2button = document.querySelector(".option-two");
+let option3button = document.querySelector(".option-three")
+let option4button = document.querySelector(".option-four")
+ /*When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables! */
+button.onclick=function(){
+
+};
+
+option1button.onclick=function(){
+option1.style.display = "block";
+};
+
+option2button.onclick=function(){
+option2.style.display = "block";
+};
+
+option3button.onclick=function(){
+  option3.style.display = "block";
+};
+
+option4button.onclick=function(){
+  option4.style.display = "block";
+};
+
+/*INSERT_VARIABLE.onclick=function(){
+
+};
+*/
+
